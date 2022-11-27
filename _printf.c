de5c9cef9c3f175e25d561ed4f77a1ffb296a253
MakgaleMampana <makgale.mampana94+alx@gmail.com>
2022-11-27 08:16:50 -0800

    my print

--- _printf.c
+++ _printf.c
@@ -1,224 +1,121 @@
 #include "main.h"
-/**
- *
- *  *_printf - prints formatted
- *
- *   *@format: the format with which the function will print
- *
- *    *Return: length of string printed
- *
- *     */
 
+/**
+ * _printf- entry
+ * @format: input
+ * Return: l
+ */
 int _printf(const char *format, ...)
-
 {
+	va_list arg;
+	int i, len = 0, bytes = 0;
 
-		va_list arg;
-
-			int i, len = 0, bytes = 0;
-
-
-
-				if (format == NULL)
-
-							return (-1);
-
-					va_start(arg, format);
-
-						for (i = 0; format[i] != '\0'; i++)
-
-								{
-
-											if (format[i] == '%' && format[i + 1] != '\0')
-
-														{
-
-																		i++;
-
-																					while (format[i] - 48 >= 0 && format[i] - 48 <= 9)
-
-																									{
-
-																														len = _isnumber(len, (format[i] - 48));
-
-																																		i++;
-
-																																					}
-
-																								bytes += _validate(_compare(format[i], len, arg), format[i], format[i - 1]);
-
-																										}
-
-													else if (format[i] == '%' && format[i + 1] == '\0')
-
-																{
-
-																				return (-1);
-
-																						}
-
-															else
-
-																		{
-
-																						bytes++;
-
-																									_putchar(format[i]);
-
-																											}
-
-																}
-
-							va_end(arg);
-
-								return (bytes);
-
+	if (format == NULL)
+	{
+		return (-1);
+	}
+	va_start(arg, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%' && format[i + 1] != '\0')
+		{
+			i++;
+			while (format[i] - 48 >= 0 && format[i] - 48 <= 9)
+			{
+				len = _isnumber(len, (format[i] - 48));
+				i++;
+			}
+			bytes += _validate(_compare(format[i], len, arg), format[i], format[i - 1]);
+		}
+		else if (format[i] == '%' && format[i + 1] == '\0')
+		{
+			return (-1);
+		}
+		else
+		{
+			bytes++;
+			_putchar(format[i]);
+		}
+	}
+	va_end(arg);
+	return (bytes);
 }
-
 /**
- *
- *  * _isnumber - length input
- *
- *   * @len: actual value of length
- *
- *    * @x: value of will be added
- *
- *     * Return: length added
- *
- *      */
-
+ * _isnumber- entry
+ * @len: input
+ * @x: input 2
+ * Return: l
+ */
 int _isnumber(int len, int x)
-
 {
-
-		len *= 10;
-
-			len += x;
-
-				return (len);
-
+	len *= 10;
+	len += x;
+	return (len);
 }
-
 /**
- *
- *  * _compare - compare parameters with format
- *
- *   * @format: actual value of format
- *
- *    * @len: number of values that must be printed
- *
- *     * @arg: list of arguments
- *
- *      * Return: 0 if dont found any parameter
- *
- *       */
-
+ * _compare- entry
+ * @format: input
+ * @len: input 2
+ * @arg: input 3
+ * Return: 0
+ */
 int _compare(char format, int len, va_list arg)
-
 {
-
-		int j, flag = 0;
-
-			form params[] = {
-
-						{'c', _char},
-
-								{'s', _str},
-
-										{'d', _int},
-
-												{'i', _int},
-
-														{'b', _binary},
-
-																{'S', _strspe},
-
-																		{'r', _rev},
-
-																				{'R', print_rot13},
-
-																						{'\0', NULL},
-
-																							};
-
-				for (j = 0; params[j].f != '\0'; j++)
-
-						{
-
-									if (params[j].f == format)
-
-												{
-
-																flag = params[j].p(arg, len);
-
-																			break;
-
-																					}
-
-											else
-
-															flag = 0;
-
-												}
-
-					return (flag);
-
+	int j, flag = 0;
+	form params[] = {
+		{'c', _char},
+		{'s', _str},
+		{'d', _int},
+		{'i', _int},
+		{'b', _binary},
+		{'S', _strspe},
+		{'r', _rev},
+		{'R', print_rot13},
+		{'\0', NULL}
+	};
+	for (j = 0; params[j].f != '\0'; j++)
+	{
+		if (params[j].f == format)
+		{
+			flag = params[j].p(arg, len);
+			break;
+		}
+		else
+		{
+			flag = 0;
+		}
+	}
+	return (flag);
 }
-
 /**
- *
- *  * _validate - valide the flag
- *
- *   * @flag: 0 if not found parameter, -1 if string is null,
- *
- *    * >= 1 flag is number of bytes
- *
- *     * @actual: actual value of format
- *
- *      * @percent: percentage
- *
- *       * Return: Number of bytes that was printed
- *
- *        */
-
+ * _validate- entry
+ * @flag: input
+ * @actual: input 2
+ * @percemt: input 3
+ * Return: b
+ */
 int _validate(int flag, char actual, char percent)
-
 {
+	int bytes = 0;
 
-		int bytes = 0;
-
-
-
-			if (flag == 0 && (actual != 'i' && actual != 'd' && actual != 'S'
-
-								    && actual != 'b'))
-
-					{
-
-								if (actual != '%')
-
-											{
-
-															_putchar(percent);
-
-																		bytes++;
-
-																				}
-
-										_putchar(actual);
-
-												bytes++;
-
-													}
-
-				if (flag == -1)
-
-							return (bytes);
-
-					if (flag >= 1)
-
-								bytes += flag;
-
-						return (bytes);
-
+	if (flag == 0 && (actual != 'i' && actual != 'd' && actual != 'S'
+		 && actual != 'b'))
+	{
+		if (actual != '%')
+		{
+			_putchar(percent);
+			bytes++;
+		}
+		_putchar(actual);
+		bytes++;
+	}
+	if (flag == -1)
+	{
+		return (bytes);
+	}
+	if (flag >= 1)
+	{
+		bytes += flag
+	}
+	return (bytes);
 }
--- main.h
+++ main.h
@@ -20,6 +20,19 @@ int _putchar(char c);
 int _isnumber(int, int);
 int _compare(char, int, va_list);
 int _validate(int, char, char);
+int _strlen(const char *s);
+int print_number(int, int);
+int hexa(int x);
+int positive(int, int, int);
+int negative(int, int, int, int);
+int print_binary(unsigned int, unsigned int);
+int _rev(va_list arg, int len);
+int print_rot13(va_list arg, int len);
 
+int _char(va_list, int);
+int _str(va_list, int);
+int _int(va_list, int);
+int _binary(va_list, int);
+int _strspe(va_list, int);
 
 #endif
